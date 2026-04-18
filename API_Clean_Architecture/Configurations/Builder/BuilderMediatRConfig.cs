58d3f28f900f127147781c59f27c79367882fe88
ColeccionaloYa Uy <162808315+ColeccionaloYaUy@users.noreply.github.com>
2026-04-17 22:40:03 -0300

    Initial commit

---
+++ API_Clean_Architecture/Configurations/Builder/BuilderMediatRConfig.cs
@@ -0,0 +1,20 @@
+﻿using API.Application.Behaviors;
+using API.Utils.Reflection;
+using System.Reflection;
+
+namespace API.API_Clean_Architecture.Configurations.Builder;
+
+public static class BuilderMediatRConfig {
+    public static void ConfigureMediatR(this IHostApplicationBuilder builder) {
+        builder.Services.AddMediatR(cnf => {
+            var assem = AppDomain.CurrentDomain.GetProjectAssemblies();
+            if (assem == null) {
+                return;
+            }
+
+            var assemblies = assem.Select(Assembly.LoadFrom).ToArray();
+            cnf.RegisterServicesFromAssemblies(assemblies);
+            cnf.AddOpenBehavior(typeof(PageValidationBehavior<,>));
+        });
+    }
+}
